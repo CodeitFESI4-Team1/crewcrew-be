00fba2768e49aea5c19fabc2f70c5dfe763ea8ec
SeoJeongYu <bbwest0709@gmail.com>
2024-10-17 20:46:38 +0900

    Initial commit

---
+++ src/main/java/com/crewcrew/CrewcrewApplication.java
@@ -0,0 +1,13 @@
+package com.crewcrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewcrewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrewcrewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crewcrew/CrewcrewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crewcrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrewcrewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
