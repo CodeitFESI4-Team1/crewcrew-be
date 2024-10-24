b4ee36550ad72c294b075445c40cb8c0137fc26f
SeoJeongYu <bbwest0709@gmail.com>
2024-10-24 20:33:12 +0900

    feat : CrewRepository 생성

---
+++ src/main/java/com/crewcrew/domain/crew/repository/CrewRepository.java
@@ -0,0 +1,9 @@
+package com.crewcrew.domain.crew.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.crewcrew.domain.crew.entity.Crew;
+
+@Repository
+public interface CrewRepository extends JpaRepository<Crew, Long> {}
