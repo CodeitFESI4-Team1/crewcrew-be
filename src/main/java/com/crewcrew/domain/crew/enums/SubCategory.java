b9a6ff0302ac39b835c658b360b9a097b058f16e
SeoJeongYu <bbwest0709@gmail.com>
2024-10-24 20:26:33 +0900

    feat : Category 및 SubCategory ENUM 생성
    - Category ENUM 추가: 크루의 주요 유형 정의
    - SubCategory ENUM 추가: 각 카테고리의 세부 유형 정의

---
+++ src/main/java/com/crewcrew/domain/crew/enums/SubCategory.java
@@ -0,0 +1,15 @@
+package com.crewcrew.domain.crew.enums;
+
+public enum SubCategory {
+  RUNNING,
+  CYCLING,
+  WEIGHT_TRAINING,
+  SOCCER,
+  BASKETBALL,
+  VOLLEYBALL,
+  TENNIS,
+  SWIMMING,
+  GOLF,
+  PILATES,
+  OTHER
+}
