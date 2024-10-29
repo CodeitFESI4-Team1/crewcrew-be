2a257adf9d44e7f13732aba4a16c65a4f3de4c17
SeoJeongYu <bbwest0709@gmail.com>
2024-10-29 18:11:49 +0900

    feat: Crew 엔티티에 크루 수정 및 참가자 수 조정 추가
    - 커밋누락 update 메서드 추가: CrewUpdateRequestDTO를 기반으로 필드 값 업데이트
    - incrementParticipantCount 메서드 추가: 참가자 수를 증가시키고, 수가 4 이상일 경우 개설확정 상태 업데이트
    - decrementParticipantCount 메서드 추가: 참가자 수를 감소시키고, 수가 4 미만일 경우 개설확정 상태 업데이트

--- src/main/java/com/crewcrew/domain/crew/entity/Crew.java
+++ src/main/java/com/crewcrew/domain/crew/entity/Crew.java
@@ -1,9 +1,11 @@
 package com.crewcrew.domain.crew.entity;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import jakarta.persistence.*;
 
+import com.crewcrew.domain.crew.dto.request.CrewUpdateRequestDTO;
 import com.crewcrew.domain.crew.enums.Category;
 import com.crewcrew.domain.crew.enums.SubCategory;
 import com.crewcrew.domain.member.entity.Member;
@@ -46,16 +48,49 @@ public class Crew extends BaseEntity {
   @Column(nullable = false)
   private int capacity;
 
+  @Builder.Default
   @Column(name = "participant_count")
   private int participantCount = 0;
 
   @Column(name = "canceled_at", nullable = true)
   private LocalDateTime canceledAt;
 
+  @Builder.Default
   @Column(name = "is_confirmed")
   private Boolean isConfirmed = false;
 
   @ManyToOne
   @JoinColumn(name = "member_id")
   private Member member;
+
+  public void update(CrewUpdateRequestDTO request) {
+    Optional.ofNullable(request.location()).ifPresent(location -> this.location = location);
+    Optional.ofNullable(request.detailedLocation())
+        .ifPresent(detailedLocation -> this.detailedLocation = detailedLocation);
+    Optional.ofNullable(request.type()).ifPresent(type -> this.type = Category.valueOf(type));
+    Optional.ofNullable(request.subType())
+        .ifPresent(subType -> this.subType = SubCategory.valueOf(subType));
+    Optional.ofNullable(request.name()).ifPresent(name -> this.name = name);
+    Optional.ofNullable(request.description())
+        .ifPresent(description -> this.description = description);
+    Optional.ofNullable(request.capacity()).ifPresent(capacity -> this.capacity = capacity);
+  }
+
+  public void incrementParticipantCount() {
+    if (this.participantCount < this.capacity) {
+      this.participantCount++;
+    }
+    if (this.participantCount > 4) {
+      this.isConfirmed = true;
+    }
+  }
+
+  public void decrementParticipantCount() {
+    if (participantCount > 0) {
+      participantCount--;
+      if (participantCount < 4) {
+        this.isConfirmed = false;
+      }
+    }
+  }
 }
