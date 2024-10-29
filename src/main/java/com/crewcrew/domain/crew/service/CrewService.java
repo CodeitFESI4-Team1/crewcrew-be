0215fb50351a8ffc29a4f1ee08404c0ff8ec72a2
SeoJeongYu <bbwest0709@gmail.com>
2024-10-29 19:35:23 +0900

    feat: 사용자 크루 탈퇴 기능 추가

--- src/main/java/com/crewcrew/domain/crew/service/CrewService.java
+++ src/main/java/com/crewcrew/domain/crew/service/CrewService.java
@@ -118,6 +118,13 @@ public class CrewService {
     return mapper.crewResponseDTO(crew, images);
   }
 
+  @Transactional
+  public void leaveCrew(Long id) {
+    Crew crew = findCrewById(id);
+    crewInfoRepository.deleteByCrewIdAndMemberId(id, getMemberId());
+    crew.decrementParticipantCount();
+  }
+
   private CrewInfo getCrewInfo(Crew crew, Member member) {
     return mapper.crewInfo(crew, member);
   }
