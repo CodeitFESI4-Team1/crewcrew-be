27c8aff3767364d06ababc4320537f7bb94755b6
SeoJeongYu <bbwest0709@gmail.com>
2024-10-30 16:17:20 +0900

    docs: 엔티티 Swagger 문서 추가

--- src/main/java/com/crewcrew/domain/member/entity/Member.java
+++ src/main/java/com/crewcrew/domain/member/entity/Member.java
@@ -8,6 +8,7 @@ import jakarta.persistence.*;
 import com.crewcrew.domain.crew.entity.Crew;
 import com.crewcrew.global.common.domain.BaseEntity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Entity
@@ -19,21 +20,26 @@ public class Member extends BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "member_id")
+  @Schema(description = "사용자 ID")
   private Long id;
 
   @Column(nullable = false, unique = true, length = 255)
+  @Schema(description = "사용자 이메일", required = true)
   private String email;
 
   @Column(nullable = false)
+  @Schema(description = "사용자 비밀번호", required = true)
   private String password;
 
   @Column(nullable = false)
+  @Schema(description = "사용자 이름", required = true)
   private String name;
 
   @Column(name = "deleted_at", nullable = true)
+  @Schema(description = "회원탈퇴 일자")
   private LocalDateTime deletedAt;
 
   @OneToMany(mappedBy = "member")
+  @Schema(description = "사용자가 참여한 크루 목록")
   private List<Crew> crews;
 }
