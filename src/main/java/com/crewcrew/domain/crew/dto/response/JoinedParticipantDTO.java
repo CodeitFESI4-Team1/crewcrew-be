d56cc224a0fccf3d38f8f6ff6e8b81b6e73bff6b
JWbase <jungwoo1002@gmail.com>
2024-11-06 18:58:04 +0900

    refactor: 불필요한 클래스 제거

--- src/main/java/com/crewcrew/domain/crew/dto/response/JoinedParticipantDTO.java
+++
@@ -1,7 +0,0 @@
-package com.crewcrew.domain.crew.dto.response;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-
-public record JoinedParticipantDTO(
-    @Schema(description = "사용자 고유 ID") Long memberId,
-    @Schema(description = "사용자 이름") String memberName) {}
