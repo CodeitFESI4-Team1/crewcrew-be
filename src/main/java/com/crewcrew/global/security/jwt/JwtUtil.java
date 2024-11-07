abb481e0c78b607520e9db458b1c5b9a06d10866
wooandrich <brad1182@naver.com>
2024-11-07 16:16:21 +0900

    chore : 환경변수 변경

--- src/main/java/com/crewcrew/global/security/jwt/JwtUtil.java
+++ src/main/java/com/crewcrew/global/security/jwt/JwtUtil.java
@@ -16,7 +16,7 @@ public class JwtUtil {
 
   private SecretKey secretKey;
 
-  public JwtUtil(@Value("${jwt.secret.key}") String secret) {
+  public JwtUtil(@Value("${jwt_secret_key}") String secret) {
     this.secretKey =
         new SecretKeySpec(
             secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
