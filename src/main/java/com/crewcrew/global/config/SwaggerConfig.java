27ac9d49ed4e4dabc2f88bcef261e1f1d04612ce
JWbase <jungwoo1002@gmail.com>
2024-11-08 13:09:24 +0900

    fix: Swagger 문서르 보기 편하도록 하나로 통합

--- src/main/java/com/crewcrew/global/config/SwaggerConfig.java
+++ src/main/java/com/crewcrew/global/config/SwaggerConfig.java
@@ -1,24 +1,24 @@
 package com.crewcrew.global.config;
 
-import java.util.List;
-
-import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 
+@OpenAPIDefinition(
+    servers = {
+      @Server(url = "https://dev.dev-crewcrew.site/", description = "개발 서버"),
+      @Server(url = "http://localhost:8080", description = "로컬 서버")
+    })
 @Configuration
 public class SwaggerConfig {
-
-  Server server = new Server().url("https://dev.dev-crewcrew.site/"); // 임시 로컬임
-
   @Bean
   public OpenAPI crewCrewApi() {
     Info info = new Info().version("1.0.0").title("CrewCrew API").description("CrewCrew API 명세서");
@@ -33,27 +33,8 @@ public class SwaggerConfig {
     SecurityRequirement securityRequirement = new SecurityRequirement().addList("JWT");
 
     return new OpenAPI()
-        .servers(List.of(server))
         .info(info)
         .addSecurityItem(securityRequirement)
         .components(new Components().addSecuritySchemes("JWT", securityScheme));
-  }
-
-  @Bean
-  public GroupedOpenApi crew() {
-    return GroupedOpenApi.builder()
-        .group("크루 기능")
-        .pathsToMatch("/api/crews/**")
-        .packagesToScan("com.crewcrew.domain.crew.controller")
-        .build();
-  }
-
-  @Bean
-  public GroupedOpenApi member() {
-    return GroupedOpenApi.builder()
-        .group("회원 기능")
-        .pathsToMatch("/auths/**")
-        .packagesToScan("com.crewcrew.domain.member.controller")
-        .build();
   }
 }
